a00887154632f01652e6ed976e714f1f40a1fae9
zzhzoe <zoe.zzh121@gmail.com>
2022-02-19 01:20:06 -0800

    first

---
+++ app.py
@@ -0,0 +1,40 @@
+from dash import Dash, html, dcc, Input, Output
+import altair as alt
+from vega_datasets import data
+
+
+# Read in global data
+movies = data.movies()
+df = movies[['Production_Budget', 'Running_Time_min', 
+             'Major_Genre', 'IMDB_Rating', 'IMDB_Votes', 'US_Gross']].dropna(subset=['Major_Genre'])
+             
+# Setup app and layout/frontend
+app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
+server = app.server
+
+app.layout = html.Div([
+    html.Iframe(
+        id='scatter',
+        style={'border-width':'0', 'width':'100%', 'height':'400px'}),
+    dcc.Slider(id='xslider', min=0, max=10),
+    dcc.Dropdown(
+            id='ycol', 
+            value='Running_Time_min',
+            options=[{'label': col, 'value': col} for col in df.columns])
+])
+
+@app.callback(
+    Output('scatter', 'srcDoc'),
+    Input('xslider', 'value'),
+    Input('ycol', 'value')
+)
+def plot_altairs(xmax, ycol, df=df.copy()):
+    chart = alt.Chart(df[df['IMDB_Rating'] < xmax]).mark_point().encode(
+        x='IMDB_Rating',
+        y=ycol,
+        color="Major_Genre",
+        tooltip=['IMDB_Rating:N']).interactive()
+    return chart.to_html()
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
